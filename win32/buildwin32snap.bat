272eb48752d5e19d18bdb156ac5d9cf5697eebb8
Fish <>
2008-03-10 09:04:38 +0000

    make sure the install compiler quits

--- win32/buildwin32snap.bat
+++ win32/buildwin32snap.bat
@@ -1,4 +1,4 @@
 @echo off
 IF EXIST win32\YahtzeeServ-Setup*.exe del win32\YahtzeeServ-Setup*.exe
-"C:\Program Files\Pantaray\QSetup\Composer.exe" win32\YahtzeeServ.qsp /Compile 
+"C:\Program Files\Pantaray\QSetup\Composer.exe" win32\YahtzeeServ.qsp /Compile /Exit
 move win32\YahtzeeServ-Setup.exe win32\YahtzeeServ-Setup-3-0-a4-%1.exe
