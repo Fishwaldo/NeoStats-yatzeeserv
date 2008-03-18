750530bcf708142016eda61f99f327173448691b
Fish <>
2008-03-18 11:49:39 +0000

    This is YahtzeeServ 3.0.0

--- win32/modconfigwin32.h
+++ win32/modconfigwin32.h
@@ -3,7 +3,7 @@
 /* #undef DEBUG */
 
 /* Version number of package */
-#define MODULE_VERSION "3.0.a4"
+#define MODULE_VERSION "3.0.0"
 
 /* Major Version */
 #define MODULE_MAJOR "3"
@@ -12,9 +12,9 @@
 #define MODULE_MINOR "0"
 
 /* Revision */
-#define MODULE_REV "a4"
+#define MODULE_REV "0"
 
 /* "Full Version" */
-#define MOD_VERSION "3.0.a4"
-#define MOD_WIN32VER "3,0,a4,0"
+#define MOD_VERSION "3.0.0"
+#define MOD_WIN32VER "3,0,0,0"
 #define MOD_WIN32VERN 3,0,0
