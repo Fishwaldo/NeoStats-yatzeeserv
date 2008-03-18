69906438d8fe87fcc0899b3498e7c8dc3507d10f
Fish <>
2008-03-18 11:56:27 +0000

    bump win32 version

--- win32/modconfigwin32.h
+++ win32/modconfigwin32.h
@@ -3,7 +3,7 @@
 /* #undef DEBUG */
 
 /* Version number of package */
-#define MODULE_VERSION "3.0.0"
+#define MODULE_VERSION "3.0.1"
 
 /* Major Version */
 #define MODULE_MAJOR "3"
@@ -12,9 +12,9 @@
 #define MODULE_MINOR "0"
 
 /* Revision */
-#define MODULE_REV "0"
+#define MODULE_REV "1"
 
 /* "Full Version" */
-#define MOD_VERSION "3.0.0"
-#define MOD_WIN32VER "3,0,0,0"
-#define MOD_WIN32VERN 3,0,0
+#define MOD_VERSION "3.0.1"
+#define MOD_WIN32VER "3,0,1,0"
+#define MOD_WIN32VERN 3,0,1
